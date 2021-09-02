6b59b64466f8086124c7a8a85b03ad12832b55f7
Tim van Osch <timvosch@pollex.nl>
2021-09-02 14:35:57 +0200

    Chore(ads1118): Update example, print to uart

--- examples/ads1118/main.c
+++ examples/ads1118/main.c
@@ -1,13 +1,16 @@
 #include <avr/io.h>
-#include "mcu/spi.h"
 #include "drivers/ads1118.h"
+#include "mcu/uart.h"
+#include "debug.h"
 
+// ADS1118 struct
+// the config fields should not be left out, they will not revert to default values!
 ads1118_t ads = {
     .cs_port = &PORTA,
     .cs_pin = PIN6,
     .config = {
         .fields = {
-            .mux = ADS1118_MUX_AIN0_GND,
+            .mux = ADS1118_MUX_AIN1_GND,
             .pga = ADS1118_PGA_4096,
             .mode = ADS1118_MODE_CONTINUOUS,
             .datarate = ADS1118_DR_128,
@@ -16,13 +19,23 @@ ads1118_t ads = {
             .nop = ADS1118_NOP_VALID,
         }}};
 
+// UART is used in this example for logging
+uart_t uart = {
+    .baudrate = 9600,
+    .config = {
+        .tx_enabled = 1,
+    }};
+
 int main(void)
 {
-  spi_init();
+  // Initialize uart for logging
+  uart_init(&uart);
+
+  // Initialize ADS1118 sensor
   ads1118_init(&ads);
 
   // Change mux
-  ads.config.fields.mux = ADS1118_MUX_AIN1_GND;
+  ads.config.fields.mux = ADS1118_MUX_AIN0_GND;
 
   // Explicitly push new configuration to the device.
   // Note that reading also updates the configuration. However, this method may be useful to put the device in
@@ -31,8 +44,10 @@ int main(void)
 
   for (;;)
   {
-    double millivolts = ads1118_read(&ads);
-    // Do something
-    // TODO: Send to UART
+    // Read millivolts from ads1118
+    float millivolts = ads1118_read(&ads);
+
+    // Print out result
+    uart_printf(&uart, "%f", millivolts);
   }
 }
