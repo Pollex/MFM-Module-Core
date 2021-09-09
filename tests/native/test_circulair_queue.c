cb7ee330b0f829b5a4e0fdeed36eee82227fc175
Tim van Osch <timvosch@pollex.nl>
2021-09-09 13:01:42 +0200

    Integrate CMAKE w/ tests

--- tests/native/test_circulair_queue.c
+++ tests/native/test_circulair_queue.c
@@ -1,8 +1,10 @@
 #include <unity.h>
 
 #define EV_QUEUE_SIZE 5
+#include "os/circulair_queue.h"
 
-#include "events.h"
+void setUp() {}
+void tearDown() {}
 
 void ev_enableInterrupts()
 {
@@ -11,9 +13,9 @@ void ev_disableInterrupts()
 {
 }
 
-void test_events_empty_queue_dequeues_nullptr(void)
+void test_events_empty_queue_dequeues_NULL(void)
 {
-  TEST_ASSERT_EQUAL(nullptr, ev_dequeue());
+  TEST_ASSERT_EQUAL(NULL, ev_dequeue());
 }
 
 void test_events_enqueue_dequeue(void)
@@ -23,46 +25,44 @@ void test_events_enqueue_dequeue(void)
   uint8_t ev3_id = 3;
   uint8_t ev4_id = 4;
 
-  ev_enqueue(ev1_id, nullptr);
-  ev_enqueue(ev2_id, nullptr);
-  ev_enqueue(ev3_id, nullptr);
-  ev_enqueue(ev4_id, nullptr);
+  ev_enqueue(ev1_id, NULL);
+  ev_enqueue(ev2_id, NULL);
+  ev_enqueue(ev3_id, NULL);
+  ev_enqueue(ev4_id, NULL);
 
   TEST_ASSERT_EQUAL_MESSAGE(ev1_id, ev_dequeue()->id, "ev_dequeue did not return first event");
   TEST_ASSERT_EQUAL_MESSAGE(ev2_id, ev_dequeue()->id, "ev_dequeue did not return second event");
   TEST_ASSERT_EQUAL_MESSAGE(ev3_id, ev_dequeue()->id, "ev_dequeue did not return third event");
   TEST_ASSERT_EQUAL_MESSAGE(ev4_id, ev_dequeue()->id, "ev_dequeue did not return fourth event");
-  TEST_ASSERT_EQUAL_MESSAGE(nullptr, ev_dequeue(), "ev_dequeue returned non-nullptr at non-existing event");
+  TEST_ASSERT_EQUAL_MESSAGE(NULL, ev_dequeue(), "ev_dequeue returned non-NULL at non-existing event");
 }
 
 void test_events_pending(void)
 {
   TEST_ASSERT_EQUAL(0, ev_pending());
 
-  ev_enqueue(0, nullptr);
+  ev_enqueue(0, NULL);
   TEST_ASSERT_EQUAL(1, ev_pending());
 
   ev_dequeue();
   TEST_ASSERT_EQUAL(0, ev_pending());
 
-  ev_enqueue(0, nullptr);
-  ev_enqueue(0, nullptr);
-  ev_enqueue(0, nullptr);
+  ev_enqueue(0, NULL);
+  ev_enqueue(0, NULL);
+  ev_enqueue(0, NULL);
   TEST_ASSERT_EQUAL(1, ev_pending());
 
-  ev_enqueue(0, nullptr);
-  ev_enqueue(0, nullptr);
-  ev_enqueue(0, nullptr);
+  ev_enqueue(0, NULL);
+  ev_enqueue(0, NULL);
+  ev_enqueue(0, NULL);
   TEST_ASSERT_EQUAL(1, ev_pending());
 }
 
 int main(int argc, char **argv)
 {
   UNITY_BEGIN();
-  RUN_TEST(test_events_empty_queue_dequeues_nullptr);
+  RUN_TEST(test_events_empty_queue_dequeues_NULL);
   RUN_TEST(test_events_enqueue_dequeue);
   RUN_TEST(test_events_pending);
-  UNITY_END();
-
-  return 0;
+  return UNITY_END();
 }
--- tests/native/test_linked_queue.c
+++ tests/native/test_linked_queue.c
@@ -1,11 +1,15 @@
 #include "unity.h"
 
+void setUp(void) {}
+void tearDown(void) {}
+
 void test_AverageThreeBytes_should_AverageMidRangeValues(void)
 {
-  TEST_ASSERT_EQUAL_HEX8(40, 40);
-  TEST_ASSERT_EQUAL_HEX8(40, 40);
-  TEST_ASSERT_EQUAL_HEX8(33, 40);
+  TEST_ASSERT_EQUAL(40, 40);
+  TEST_ASSERT_EQUAL(40, 40);
+  TEST_ASSERT_EQUAL(33, 40);
 }
+
 int main(void)
 {
   UNITY_BEGIN();
