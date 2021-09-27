95cf5877975979e901ec2fb2b64e68c1f262bd9d
Tim van Osch <timvosch@pollex.nl>
2021-09-27 17:23:59 +0200

    Chore(uart): Add example

---
+++ examples/uart/main.c
@@ -0,0 +1,31 @@
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+
+#include "mcu/uart.h"
+
+#define LED_PORT PORTA
+#define LED_PIN PIN6
+#define SENSOR_PWR_PORT PORTA
+#define SENSOR_PWR_PIN PIN5
+
+uart_t uart = {
+    .baudrate = 9600,
+    .config = {
+        .rx_enabled = 1,
+        .tx_enabled = 1,
+    },
+};
+
+int main(void)
+{
+  // Initialize uart
+  uart_init(&uart);
+
+  // Infinite loop
+  for (;;)
+  {
+    // Receive a character on uart, and then immediatly transmit it (echo).
+    uart_putc(&uart, uart_getc(&uart));
+  }
+}
